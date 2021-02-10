85e1e618080313b19c067433d4a756505052488d
Jordan <jordanking7@gmail.com>
2021-02-10 12:06:56 -0700

    webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+//require a path
+const path = require('path')
+
+module.exports = {
+    entry : './client/index.js',
+    output : {
+      path : path.resolve(__dirname, 'build'),
+      filename: 'bundle.js'
+    },
+    mode: process.env.NODE_ENV,
+    module : {
+      rules: [
+        { test: /\.jsx?/, 
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options : {presets: ['@babel/preset-env', '@babel/preset-react' ]
+          }
+         }
+        }, {
+          test: /\.s[ac]ss$/i,
+          use: 'scss-loader'
+        }
+      ]
+    },
+    devServer: {
+        publicPath: '/build',
+        proxy: {
+          '/': 'http://localhost:3000',
+        },
+        hot: true,
+      }
+}
